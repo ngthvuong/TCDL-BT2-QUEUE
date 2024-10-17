2f52213f48f3ae4494b89bf2ff56ea53ee7d4013
Vuong Nguyen <ngthvuong@gmail.com>
2024-10-17 23:23:11 +0700

    format code

--- main.cpp
+++ main.cpp
@@ -3,89 +3,7 @@
 
 using namespace std;
 
-void queueScreen(Queue *queue)
-{
-    cout << "--------------------" << endl;
-    cout << "Pick an action on queue" << endl;
-    cout << " - 0 : Return Main Screen" << endl;
-    cout << " - 1 : Enqueue" << endl;
-    cout << " - 2 : Dequeue" << endl;
-    cout << " - 3 : Peek " << endl;
-    cout << " - 4 : Show Queue " << endl;
-
-    int queueLoop = 0;
-    do
-    {
-        cout << "--------------------" << endl;
-        cout << "Action = ";
-        cin >> queueLoop;
-
-        switch (queueLoop)
-        {
-        case 0:
-            cout << "--------------------" << endl;
-            cout << "Commands of main screen" << endl;
-            cout << " - 0 : Exit program" << endl;
-            cout << " - 1 : Array Queue" << endl;
-            cout << " - 2 : Linked List Queue" << endl;
-            break;
-        case 1:
-            try
-            {
-
-                cout << "--------------------" << endl;
-
-                cout << "Integer = ";
-                int data;
-                cin >> data;
-                queue->enQueue(data);
-                cout << "Enqueue successfully!" << endl;
-                ;
-            }
-            catch (out_of_range &e)
-            {
-                cout << "Errors: " << e.what() << endl;
-            }
-            break;
-        case 2:
-            try
-            {
-                cout << "--------------------" << endl;
-                cout << "Dequeue integer = " << queue->deQueue() << endl;
-            }
-            catch (out_of_range &e)
-            {
-                cout << "Errors: " << e.what() << endl;
-            }
-            break;
-        case 3:
-            try
-            {
-                cout << "--------------------" << endl;
-                cout << "Peek integer = " << queue->peek() << endl;
-            }
-            catch (out_of_range &e)
-            {
-                cout << "Errors: " << e.what() << endl;
-            }
-            break;
-        case 4:
-            try
-            {
-                cout << "--------------------" << endl;
-                cout << "List Queue: " << endl;
-                queue->printQueue();
-            }
-            catch (out_of_range &e)
-            {
-                cout << "Errors: " << e.what() << endl;
-            }
-            break;
-        default:
-            cout << "Wrong symbol, type again" << endl;
-        }
-    } while (queueLoop);
-}
+void queueScreen(Queue *);
 
 int main()
 {
@@ -137,4 +55,86 @@ int main()
         delete queue;
     }
     return 0;
+}
+
+void queueScreen(Queue *queue)
+{
+    cout << "--------------------" << endl;
+    cout << "Pick an action on queue" << endl;
+    cout << " - 0 : Return Main Screen" << endl;
+    cout << " - 1 : Enqueue" << endl;
+    cout << " - 2 : Dequeue" << endl;
+    cout << " - 3 : Peek " << endl;
+    cout << " - 4 : Show Queue " << endl;
+
+    int queueLoop = 0;
+    do
+    {
+        cout << "--------------------" << endl;
+        cout << "Action = ";
+        cin >> queueLoop;
+
+        switch (queueLoop)
+        {
+        case 0:
+            cout << "--------------------" << endl;
+            cout << "Commands of main screen" << endl;
+            cout << " - 0 : Exit program" << endl;
+            cout << " - 1 : Array Queue" << endl;
+            cout << " - 2 : Linked List Queue" << endl;
+            break;
+        case 1:
+            try
+            {
+                cout << "--------------------" << endl;
+                cout << "Integer = ";
+                int data;
+                cin >> data;
+
+                queue->enQueue(data);
+                cout << "Enqueue successfully!" << endl;
+            }
+            catch (out_of_range &e)
+            {
+                cout << "Errors: " << e.what() << endl;
+            }
+            break;
+        case 2:
+            try
+            {
+                cout << "--------------------" << endl;
+                cout << "Dequeue integer = " << queue->deQueue() << endl;
+            }
+            catch (out_of_range &e)
+            {
+                cout << "Errors: " << e.what() << endl;
+            }
+            break;
+        case 3:
+            try
+            {
+                cout << "--------------------" << endl;
+                cout << "Peek integer = " << queue->peek() << endl;
+            }
+            catch (out_of_range &e)
+            {
+                cout << "Errors: " << e.what() << endl;
+            }
+            break;
+        case 4:
+            try
+            {
+                cout << "--------------------" << endl;
+                cout << "List Queue: " << endl;
+                queue->printQueue();
+            }
+            catch (out_of_range &e)
+            {
+                cout << "Errors: " << e.what() << endl;
+            }
+            break;
+        default:
+            cout << "Wrong symbol, type again" << endl;
+        }
+    } while (queueLoop);
 }
